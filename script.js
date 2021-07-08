478d82477e1d891932f0fe56cb2ca4a90117996d
Arman Arif <arman3472rmn@gmail.com>
2021-07-08 14:48:52 +0600

    Random quore generator greated

---
+++ script.js
@@ -0,0 +1,12 @@
+const quote = {
+    content: "",
+    author: "",
+    getQuote() {
+        fetch("https://api.quotable.io/random")
+        .then(res=>res.json())
+        .then(data=>{
+            this.content = data.content
+            this.author = "-- " + data.author
+        })
+    }
+}
